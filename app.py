dbf304f63d81e6df6be886e112cabd9e9636bdd4
Asuthosh <asuthoshazad@gmail.com>
2018-10-29 22:07:08 +0530

    flask sample

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
-
+from flask import Flask, render_template,request
+from data import Faculties
 app=Flask(__name__)
 
-
+getfaculties = Faculties()
 @app.route('/')
 def index():
-    return 'hello'
+   return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return '<h1> about </h1>'
+  #  return '<h1> about </h1>'
+  return render_template('about.html')
+
+@app.route('/send', methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getname=request.form['name']
+        getemail=request.form['email']
+        return render_template('contact.html',name=getname,email=getemail)
+
+@app.route('/faculties')
+def faculties():
+    return render_template('faculties.html',facultydata=getfaculties)
 
 if(__name__=='__main__'):
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,22 @@
+def Faculties():
+    faculties=[
+        {
+            'dept':'Computer Science',
+            'name':'Ramesh',
+            'mob':'9999999999',
+            'email':'ramesh@gmail.com'
+        },
+        {
+            'dept':'Electical',
+            'name':'Gokul',
+            'mob':'8888888888',
+            'email':'gokul@gmail.com'
+        },
+        {
+            'dept':'Electronics',
+            'name':'Ajith Kumar',
+            'mob':'9000000000',
+            'email':'ajith@gmail.com'
+        }
+    ]
+    return faculties
